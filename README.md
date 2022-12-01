be47404843c18e7f8a341e470a4887b2979c6bd4
Anthony Sottile <asottile@umich.edu>
2022-12-01 00:05:10 -0500

    day 1 part 1

--- README.md
+++ README.md
@@ -21,4 +21,7 @@ for 2022, I'm planning to implement in python
 
 ```console
 $ find -maxdepth 1 -type d -name 'day*' -not -name day00 | sort | xargs --replace bash -xc 'python {}/part1.py {}/input.txt; python {}/part2.py {}/input.txt'
++ python day01/part1.py day01/input.txt
+68787
+> 756 μs
 ```
---
+++ day01/part1.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    return max(
+        sum(int(line) for line in part.splitlines())
+        for part in s.split('\n\n')
+    )
+
+
+INPUT_S = '''\
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+'''
+EXPECTED = 24000
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
